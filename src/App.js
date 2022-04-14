1e708ee3d6780d8d0c1900666ef86719bcfddd1a
Joyal Thomas <joyal@neoito.com>
2022-04-14 23:35:30 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import candidate_listing from './candidate_listing'
+export default function App() {
+  return (
+    <div>
+      <candidate_listing/>
+    </div>
+  )
+}
